0be50a71c81e3d6987e9e33dbc40f1d721b9d2bb
tomKPZ <tomKPZ@gmail.com>
2023-01-26 09:16:28 -0800

    Add missing QWERTY keys

--- keyboards/handwired/dctl/keymaps/default/keymap.c
+++ keyboards/handwired/dctl/keymaps/default/keymap.c
@@ -25,6 +25,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                            XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX,
     //                            XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX),
 
+    [_QWERTY] = LAYOUT(
+                      KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_1,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_0,
+                      KC_MINS, KC_EQL,                                          KC_LBRC, KC_RBRC,
+                               KC_BSPC, OS_LSFT, OS_SYM,      KC_ENT,  KC_SPC,  OS_NAV,
+                               OS_LALT, REPEAT,  KC_ESC,      OS_LGUI, OS_LCTL, KC_UNDS),
+
     [_APT] = LAYOUT(
                       KC_SCLN, KC_EQL,  KC_SLSH, KC_TAB,      KC_DEL,  KC_ASTR, KC_RPRN, KC_LPRN,
     KC_LBRC, KC_X,    KC_C,    KC_D,    KC_L,    KC_V,        KC_MINS, KC_F,    KC_O,    KC_U,    KC_Q,    KC_RBRC,
@@ -34,31 +43,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_BSPC, OS_LSFT, OS_SYM,      KC_ENT,  KC_SPC,  OS_NAV,
                                OS_LALT, REPEAT,  KC_ESC,      OS_LGUI, OS_LCTL, KC_UNDS),
 
-    [_QWERTY] = LAYOUT(
-                      KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      KC_LEFT, KC_RGHT,                                         KC_DOWN, KC_UP,
-                               KC_BSPC, OS_LSFT, OS_SYM,      KC_ENT,  KC_SPC,  OS_NAV,
-                               OS_LALT, REPEAT,  KC_ESC,      OS_LGUI, OS_LCTL, KC_UNDS),
-
     [_SYMBOL] = LAYOUT(
                       _______, _______, _______, _______,     _______, _______, _______, _______,
     _______, _______, KC_2,    KC_3,    KC_4,    XXXXXXX,     _______, KC_7,    KC_8,    KC_9,    XXXXXXX, _______,
     _______, KC_1,    KC_AT,   KC_HASH, KC_DLR,  KC_5,        KC_6,    KC_AMPR, KC_GRV,  KC_TILD, KC_0,    _______,
     _______, KC_EXLM, KC_QUES, KC_PLUS, KC_PERC, XXXXXXX,     XXXXXXX, KC_CIRC, _______, _______, _______, _______,
                       _______, _______,                                         _______, _______,
-                               OS_FUNC, OS_LSFT, OS_SYM,      OS_SYM,  OS_LSFT, OS_FUNC,
+                               OS_FUNC, OS_LSFT, XXXXXXX,     XXXXXXX, OS_LSFT, OS_FUNC,
                                OS_LALT, OS_LCTL, OS_LGUI,     OS_LGUI, OS_LCTL, OS_LALT),
 
     [_NAVIGATION] = LAYOUT(
                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_CAPS, KC_PSCR, KC_INS,  XXXXXXX, XXXXXXX,
-    XXXXXXX, KC_HOME, KC_END,  KC_LEFT, KC_RGHT, XXXXXXX,     XXXXXXX, KC_DOWN, KC_UP,   KC_PGDN, KC_PGUP, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP,     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_INS,  KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX,
                       XXXXXXX, XXXXXXX,                                         XXXXXXX, XXXXXXX,
-                               OS_NAV,  OS_LSFT, OS_FUNC,     OS_FUNC, OS_LSFT, OS_NAV,
+                               XXXXXXX, OS_LSFT, OS_FUNC,     OS_FUNC, OS_LSFT, XXXXXXX,
                                OS_LALT, OS_LCTL, OS_LGUI,     OS_LGUI, OS_LCTL, OS_LALT),
 
     [_FUNCTION] = LAYOUT(
